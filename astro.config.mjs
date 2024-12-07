4697ea464aebcb72ffd82a8eba284bfe993bbd59
LeFede <lefedeok@gmail.com>
2024-12-07 18:36:01 -0300

    go

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,27 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+
+import node from "@astrojs/node";
+import netlify from "@astrojs/netlify";
+
+import react from "@astrojs/react";
+import tailwind from "@astrojs/tailwind";
+
+let adapter;
+
+if (process.argv[3] == "--target=node") adapter = node({ mode: "standalone" });
+else adapter = netlify();
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [react(), tailwind()],
+  output: "static",
+
+  env: {
+    schema: {
+      // TAL: envField.string({ context: "server", access: "public" }),
+    },
+  },
+
+  adapter,
+});
---
+++ src/components/EdisonMarket/edisonMarketAdapter.ts
@@ -0,0 +1,34 @@
+import type { Course } from "../../env";
+import type { AdaptedCourse } from "./types";
+
+export const edisonMarketAdapter = (course: Course): AdaptedCourse => {
+  const href = `curso/${course.urlLanding}`;
+  const { startTime } = course?.events?.[0] || { startTime: "" };
+  const { htmlColor: color, value: pill } = course?.category || {};
+  const pics = course?.users?.map((user) => user.user.picture) || [];
+  const usersNames =
+    course?.users?.map((user) => user.user.name).join(" y ") || "";
+  const usersJobs = course.users
+    ?.map(
+      (user: any) =>
+        `${user.user?.jobTitle ? `${user.user.jobTitle} ` : ""}${
+          user.user?.company ? `@${user.user?.company}` : ""
+        }`
+    )
+    .filter((job) => job.trim() !== "")
+    .join(" y ");
+
+  const { comingSoon, name } = course;
+
+  return {
+    href,
+    startTime,
+    color,
+    pill,
+    pics,
+    usersNames,
+    usersJobs,
+    comingSoon,
+    name,
+  } as const;
+};
